d30e0532864933ffce4584d90e6735d25721c8a5
Luciano Rodrigues <lukrodrigues68@hotmail.comclear$ git config --global user.email lukrodrigues68@hotmail.comexitcdexit>
2024-09-25 11:40:02 -0300

    1 e 2 respostas de query

--- schemaquery.sql
+++ schemaquery.sql
@@ -6,3 +6,13 @@ SELECT id_vendedor, nome, salario
 FROM vendedores
 WHERE inativo = 'false'
 ORDER BY nome ASC;
+
+--2)Escreva uma query para listar os funcionários que possuem um salário acima da 
+--média salarial de todos os funcionários. A consulta deve mostrar as colunas id, nome, 
+--e salario, ordenadas pelo salario em ordem descendente.
+
+SELECT id_vendedor, nome, salario
+FROM vendedores
+WHERE salario > (SELECT AVG(salario) FROM vendedores)
+ORDER BY salario DESC;
+

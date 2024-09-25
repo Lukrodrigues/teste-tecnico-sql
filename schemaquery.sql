f2fd218477990b846a627f9ffe755277c4048b25
Luciano Rodrigues <lukrodrigues68@hotmail.comclear$ git config --global user.email lukrodrigues68@hotmail.comexitcdexit>
2024-09-25 12:28:03 -0300

    respostas 3,4,5

--- schemaquery.sql
+++ schemaquery.sql
@@ -16,3 +16,53 @@ FROM vendedores
 WHERE salario > (SELECT AVG(salario) FROM vendedores)
 ORDER BY salario DESC;
 
+--3) Escreva uma query para listar todos os clientes e o valor total de pedidos já 
+--transmitidos. A consulta deve retornar as colunas id, razao_social, total, ordenadas
+-- pelo total em ordem descendente.
+
+SELECT t.id_cliente AS id, t.razao_social, COALESCE(CAST(SUM(p.valor_total) AS INT), 0) AS total
+FROM clientes t
+LEFT JOIN pedido p ON t.id_cliente = p.id_cliente
+GROUP BY t.id_cliente, t.razao_social
+ORDER BY total DESC;
+
+--4)Escreva uma query que retorne a situação atual de cada pedido da base. A consulta deve retornar as colunas id, valor, data e situacao. A situacao deve obedecer a seguinte regra:
+--Se possui data de cancelamento preenchido: CANCELADO
+--Se possui data de faturamento preenchido: FATURADO
+--Caso não possua data de cancelamento e nem faturamento: PENDENTE
+
+SELECT 
+    s.id_pedido AS id,
+    s.valor_total AS valor,
+    s.data_emissao AS data,
+    CASE 
+        WHEN s.data_cancelamento IS NOT NULL THEN 'CANCELADO'
+        WHEN s.data_faturamento IS NOT NULL THEN 'FATURADO'
+        ELSE 'PENDENTE'
+    END AS situacao
+FROM pedido s;
+
+--5-Escreva uma query que retorne o produto mais vendido ( em quantidade ), incluindo 
+--o valor total vendido deste produto, quantidade de pedidos em que ele apareceu e 
+--para quantos clientes diferentes ele foi vendido. A consulta deve retornar as 
+--colunas id_produto, quantidade_vendida, total_vendido, clientes, pedidos. 
+--Caso haja empate em quantidade de vendas, utilizar o total vendido como critério de 
+--desempate.
+
+
+SELECT 
+    p.id_produto,
+    CAST(SUM(pv.quantidade) AS INT) AS quantidade_vendida,
+    CAST(SUM(pv.preco_praticado * pv.quantidade) AS INT) AS total_vendido,
+    COUNT(DISTINCT pv.id_pedido) AS pedidos,
+    COUNT(DISTINCT ped.id_cliente) AS clientes
+FROM itens_pedido pv
+JOIN pedido ped ON pv.id_pedido = ped.id_pedido
+JOIN produtos p ON pv.id_produto = p.id_produto
+GROUP BY p.id_produto
+ORDER BY quantidade_vendida DESC, total_vendido DESC
+LIMIT 1;
+
+
+
+
